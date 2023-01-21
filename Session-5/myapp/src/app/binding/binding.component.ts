0049704a717bb81c29ba630a2e72a000ec8cc7df
unknown <ganitniduniyanikunj10@gmail.com>
2023-01-21 10:40:00 +0530

    Angular App and components,Bindings-Interpolation and Property Bindings

--- Session-5/myapp/src/app/binding/binding.component.ts
+++ Session-5/myapp/src/app/binding/binding.component.ts
@@ -13,5 +13,8 @@ products=[{name:"pencile",price:"8.3",available:"21-01-2023",ratings:4.8},
 {name:"scale",price:"4.5",available:"27-01-2023",ratings:5.0},
 {name:"books",price:"11.5",available:"29-01-2023",ratings:4.5},];
 
+name:string="NIKUNJ SONI";
+url:string="http://www.google.com/";
+
 }
 
