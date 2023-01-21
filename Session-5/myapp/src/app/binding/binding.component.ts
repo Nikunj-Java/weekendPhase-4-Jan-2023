b444c6ab49a95ed5cfefd96fd44de87150bebb14
unknown <ganitniduniyanikunj10@gmail.com>
2023-01-21 10:14:25 +0530

    Angular App and components,CLass Bindings

---
+++ Session-5/myapp/src/app/binding/binding.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-binding',
+  templateUrl: './binding.component.html',
+  styleUrls: ['./binding.component.css']
+})
+export class BindingComponent {
+
+products=[{name:"pencile",price:"8.3",available:"21-01-2023",ratings:4.8},
+{name:"pen",price:"10",available:"25-01-2023",ratings:4.6},
+{name:"eraser",price:"5.0",available:"26-01-2023",ratings:4.9},
+{name:"scale",price:"4.5",available:"27-01-2023",ratings:5.0},
+{name:"books",price:"11.5",available:"29-01-2023",ratings:4.5},];
+
+}
+
