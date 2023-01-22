1c995598feffdabf09954cd4613119c832070158
unknown <ganitniduniyanikunj10@gmail.com>
2023-01-22 10:32:07 +0530

    ReactiveForms,Service and Injectables

--- Session-6/formApp/src/app/app.module.ts
+++ Session-6/formApp/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
+import { ServiceComponent } from './service/service.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
---
+++ Session-6/formApp/src/app/service/service.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-service',
+  templateUrl: './service.component.html',
+  styleUrls: ['./service.component.css']
+})
+export class ServiceComponent implements OnInit {
+
+  title='service';
+  user={"fname":'',"lname":''};
+
+  //dependency injection
+  constructor(private service:DataService){}
+  
+  ngOnInit(): void {
+    this.user=this.service.getUser();
+  }
+
+}
